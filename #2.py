6baf1a1fed620eaff83739d7e4feeddfa7d5c20f
vsanders <vsanders@VysSurfaceBook.localdomain>
2018-11-08 19:00:19 -0500

    Added file for problem #2

--- #2.py
+++ #2.py
@@ -18,8 +18,7 @@ data.groupby(["region"])["observations"].mean()
 
 #scatter plot of all observations 
 b=ggplot(data,aes(x="factor(region)",y="observations"))
-b+geom_point()+coord_cartesian()+theme_classic()+xlab("region")+ylab("observations")+geom_jitter()
-b+geom_jitter()+coord_cartesian()+theme_classic()+xlab("region")+ylab("observations")
+b+geom_jitter(alpha=.1)+coord_cartesian()+theme_classic()+xlab("region")+ylab("observations")
 
 #the bar and scatter plot tell different stories
 #the barplot shows the mean of observations for each region
